11b2e0c0a0df681872997a1074a6ff87f4f4b218
camalzade_elvin <camalzade_elvin@outlook.com>
2019-04-30 19:46:27 +0400

    ViewModel side

---
+++ ListView/Entities/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView.Entities
+{
+    class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Subject Subject { get; set; }
+        public int Level { get; set; }
+        public Filial Filial { get; set;}
+        public string Note { get; set; }
+    }
+}
---
+++ ListView/ViewModel/GroupViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ListView.ViewModel
+{
+    class GroupViewModel : BaseViewModel
+    {
+
+        public Group CurrentGroup { get; set; }
+        private List<Group> allgroups;
+        public List<Group> AllGroups
+        {
+            get
+            {
+                return allgroups;
+            }
+            set
+            {
+                allgroups = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("AllGroups"))
+            }
+        }
+
+    }
+
+
+}
