394e76c287ff681d6c9575f4c79fe3fff26b240d
camalzade_elvin <camalzade_elvin@outlook.com>
2019-05-05 16:04:49 +0400

    I have to add refresh list button . . .

--- ListView/Commands/ShowSecondView.cs
+++ ListView/Commands/ShowSecondView.cs
@@ -17,10 +17,10 @@ namespace ListView.Commands
         {
             return true;
         }
-
+        public int IdCounter { get; set; }
         public void Execute(object parameter)
         {
-
+            IdCounter = 0;
             /*      public int Id { get; set; }
         public string Name { get; set; }
         public Subject Subject { get; set; }
@@ -35,46 +35,46 @@ namespace ListView.Commands
             {
                 new Entities.Filial()
                 {
-                    Id=1,Name="Baku"
+                    Id=++IdCounter,Name="Baku"
                 },
                 new Entities.Filial()
                 {
-                    Id=2,Name="Sumqayit"
+                    Id=++IdCounter,Name="Sumqayit"
                 },
                 new Entities.Filial(){
-                    Id=3,Name="Khirdalan"
+                    Id=++IdCounter,Name="Khirdalan"
                 }
             };
             groupViewModel.Levels = new List<Entities.Level>()
             {
                 new Entities.Level()
                 {
-                    Id=5,Name="Beginner"
+                    Id=++IdCounter,Name="Beginner"
                 },
                 new Entities.Level()
                 {
-                    Id=6,Name="Elementary"
+                    Id=++IdCounter,Name="Elementary"
                 },
                 new Entities.Level()
                 {
-                    Id=7,Name="Pre-Intermediate"
+                    Id=++IdCounter,Name="Pre-Intermediate"
                 }
             };
             groupViewModel.Subjects = new List<Entities.Subject>() {
 
                 new Entities.Subject()
                 {
-                    Id=1,
+                    Id=++IdCounter,
                     Name="IT"
                 },
                 new Entities.Subject()
                 {
-                    Id=2,
+                    Id=++IdCounter,
                     Name="Programming"
                 },
                 new Entities.Subject(){
 
-                    Id=3,
+                    Id=++IdCounter,
                     Name="Design"
                 }
             };
@@ -82,16 +82,32 @@ namespace ListView.Commands
             {
                 new Entities.Group()
                 {
-                    Id=1,Name="Elvin", Subject=new Entities.Subject(){
-                        Id=6,Name=groupViewModel.Subjects[0].Name
+                    Id=++IdCounter,Name="Arif", Subject=new Entities.Subject(){
+                        Id=++IdCounter,Name=groupViewModel.Subjects[0].Name
                     },
                     Level=new Entities.Level()
                     {
-                        Id=1,Name=groupViewModel.Levels[0].Name
+                        Id=++IdCounter,Name=groupViewModel.Levels[0].Name
                     }
                     ,Filial=new Entities.Filial(){
 
-                        Id=5,Name=groupViewModel.Filials[0].Name
+                        Id=++IdCounter,Name=groupViewModel.Filials[0].Name
+                    },Note="Everything is okay ."
+
+
+                },
+                new Entities.Group()
+                {
+                    Id=++IdCounter,Name="Tural", Subject=new Entities.Subject(){
+                        Id=++IdCounter,Name=groupViewModel.Subjects[1].Name
+                    },
+                    Level=new Entities.Level()
+                    {
+                        Id=++IdCounter,Name=groupViewModel.Levels[1].Name
+                    }
+                    ,Filial=new Entities.Filial(){
+
+                        Id=++IdCounter,Name=groupViewModel.Filials[1].Name
                     },Note="Everything is okay ."
 
 
@@ -99,6 +115,38 @@ namespace ListView.Commands
                 new Entities.Group()
                 {
                     Id=1,Name="Namiq", Subject=new Entities.Subject(){
+                        Id=6,Name=groupViewModel.Subjects[1].Name
+                    },
+                    Level=new Entities.Level()
+                    {
+                        Id=1,Name=groupViewModel.Levels[1].Name
+                    }
+                    ,Filial=new Entities.Filial(){
+
+                        Id=5,Name=groupViewModel.Filials[1].Name
+                    },Note="Everything is okay ."
+
+
+                },
+                new Entities.Group()
+                {
+                    Id=1,Name="Togrul", Subject=new Entities.Subject(){
+                        Id=6,Name=groupViewModel.Subjects[1].Name
+                    },
+                    Level=new Entities.Level()
+                    {
+                        Id=1,Name=groupViewModel.Levels[1].Name
+                    }
+                    ,Filial=new Entities.Filial(){
+
+                        Id=5,Name=groupViewModel.Filials[1].Name
+                    },Note="Everything is okay ."
+
+
+                },
+                new Entities.Group()
+                {
+                    Id=1,Name="Israfil", Subject=new Entities.Subject(){
                         Id=6,Name=groupViewModel.Subjects[1].Name
                     },
                     Level=new Entities.Level()
